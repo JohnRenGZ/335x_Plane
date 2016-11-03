7e7886ee4a732aed083cf20d9a742e14bf270f53
JohnRenGZ <ren_yj_gz@163.com>
2016-11-03 16:59:36 +0800

    create project

---
+++ src/335x_Plane.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : 335x_Plane.cpp
+// Author      : JohnRen
+// Version     :
+// Copyright   : JohnRen
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
